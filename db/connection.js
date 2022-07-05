e77475f02f7713ae40b884875ffcfc4dbb53540c
Samantha-Ruth <sam.haberman@gmail.com>
2022-07-05 09:05:32 -0600

    made sure dependencies were correct and started method to hide password

--- db/connection.js
+++ db/connection.js
@@ -10,3 +10,14 @@ const db = mysql.createConnection({
   });
 
 module.exports = db;
+
+// // Would this work to protect password? 
+// const db = mysql.createConnection({
+//   host: 'localhost',
+//   // Your MySQL username,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
+
+// module.exports = db;
